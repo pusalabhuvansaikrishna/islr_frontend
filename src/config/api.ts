8bce02dac712e186d784f9d7316cb9f61811bbbb
Bhuvan Sai Krishna <pusalabhuvansaikrishna@gmail.com>
2026-09-06 22:12:28 +0530

    Updated path

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";
+/*export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:8000";*/
+
+export const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://10.4.16.36:8007";
